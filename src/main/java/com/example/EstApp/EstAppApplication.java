d30caf91db68b4c419250dbee15366e10f2ccc90
Diana Smirnova <dianaart1997@gmail.com>
2023-12-27 17:41:00 +0200

    add spring+gradle

--- src/main/java/com/example/EstApp/EstAppApplication.java
+++ src/main/java/com/example/EstApp/EstAppApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EstAppApplication {
 
 
-	test	public static void main(String[] args) {
+
+	public static void main(String[] args) {
 		SpringApplication.run(EstAppApplication.class, args);
 	}
 
