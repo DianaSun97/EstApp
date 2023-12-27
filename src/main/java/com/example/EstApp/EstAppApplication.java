bb34e0749a1ad867b02b1e3c18b668e6a894867c
Diana Smirnova <dianaart1997@gmail.com>
2023-12-27 17:30:09 +0200

    add spring+gradle

---
+++ src/main/java/com/example/EstApp/EstAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.EstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EstApp/EstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EstApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
