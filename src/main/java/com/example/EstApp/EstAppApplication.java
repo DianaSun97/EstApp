8ba8765265122d463b72b32c63bdb1af3248c886
Diana Smirnova <dianaart1997@gmail.com>
2023-12-27 22:57:12 +0200

    added react

--- src/main/java/com/example/EstApp/EstAppApplication.java
+++ src/main/java/com/example/EstApp/EstAppApplication.java
@@ -2,6 +2,8 @@ package com.example.EstApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 public class EstAppApplication {
@@ -12,4 +14,8 @@ public class EstAppApplication {
 		SpringApplication.run(EstAppApplication.class, args);
 	}
 
+	@GetMapping(path = "/nominative/{word}")
+	public String getNominative(@PathVariable String word) {
+
+	}
 }
